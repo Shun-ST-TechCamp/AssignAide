821c6d81ec5dd12bd150c7844e7cb64a8aa894a5
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-12 02:09:08 +0900

    ビューファイルの抜けを修正

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -13,6 +13,7 @@ class CastsController < ApplicationController
     if @cast.update(cast_params)
       redirect_to casts_path, notice: 'キャストの情報を更新しました。'
     else
+      Rails.logger.info @cast.errors.full_messages
       render :edit
     end
   end
@@ -20,6 +21,6 @@ class CastsController < ApplicationController
   private
 
   def cast_params
-    params.require(:cast).permit(:first_name, :family_name, :company_id, :health, :sara_shiwake_skill_id, :sara_arai_skill_id, :sara_nagashi_skill_id, :sara_huki_skill_id, :kigu_arai_skill_id, :kigu_nagashi_skill_id, :silver_migaki_skill_id)
+    params.require(:cast).permit(:first_name, :family_name, :company_id, :health, :sara_shiwake_skill_id, :sara_arai_skill_id, :sara_nagashi_skill_id, :sara_huki_skill_id, :kigu_arai_skill_id, :kigu_nagashi_skill_id, :kigu_huki_skill_id, :silver_migaki_skill_id)
   end
 end
