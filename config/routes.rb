36c18bdd2a8872b3ab09a8695c3ead6692dd1a02
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-13 09:46:28 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :casts, controllers: { registrations: 'custom_registrations' }
   root to: 'positions#index'
   resources :casts, only: [:index, :new, :create, :edit, :update]
-  resources :positions, only: [:index]
+  resources :positions, only: [:index, :new, :create]
 end
