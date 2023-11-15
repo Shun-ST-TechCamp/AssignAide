ed14c90f4166f0c5f11865ba5d6a5cb8d4c4f4fe
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-15 14:29:40 +0900

    スケジュールコントローラー、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'positions#index'
   resources :casts, only: [:index, :new, :create, :edit, :update]
   resources :positions, only: [:index]
+  resources :schedules, only: [:index]
 end
