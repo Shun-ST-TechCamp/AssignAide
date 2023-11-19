4218641d47ef32290e59e5b77c4fc7fe21d449de
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-19 20:20:59 +0900

    faker・jimei インストール・seedファイルで、キャストを20人追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,29 @@
 #   { position_name: 'silver_migaki', capacity: 1, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
 #   { position_name: 'extra', capacity: 99, fatigue_level: 0, position_type: 2, required_skill_level: 1 }
 # ])
+
+require 'faker'
+require 'gimei'
+
+Faker::Config.locale = 'ja'
+
+10.times do
+  person = Gimei.name
+
+  Cast.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+    first_name: person.first.katakana,
+    family_name: person.last.katakana,
+    company_id: Faker::Number.number(digits: 8),
+    health: 100,
+    sara_shiwake_skill_id: rand(2..4),
+    sara_arai_skill_id: rand(2..4),
+    sara_nagashi_skill_id: rand(2..4),
+    sara_huki_skill_id: rand(2..4),
+    kigu_arai_skill_id: rand(2..4),
+    kigu_nagashi_skill_id: rand(2..4),
+    kigu_huki_skill_id: rand(2..4),
+    silver_migaki_skill_id: rand(2..4)
+  )
+end
