89f2e897533a2a8ec864177786ee53a407fd684a
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-19 21:57:09 +0900

    ビューファイルの表示変更

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,14 +1,16 @@
 class PositionsController < ApplicationController
   def index
     @date = params[:date] || Date.today
+    @positions = Position.all
     schedules = Schedule.includes(:position, :cast, :workday)
                         .where(workdays: { date: @date })
                         .order(:start_time)
 
-    @schedules_by_time_slot = schedules.each_with_object({}) do |schedule, hash|
-      time_slot = schedule.time_slot
-      hash[time_slot] ||= []
-      hash[time_slot] << schedule
+    @schedules_by_time_slot = Schedule::TIME_SLOTS.keys.each_with_object({}) do |time_slot, hash|
+      start_time, end_time = Schedule::TIME_SLOTS[time_slot][1..2]
+      hash[time_slot] = schedules.select do |schedule|
+        schedule.start_time.strftime("%H:%M") == start_time && schedule.end_time.strftime("%H:%M") == end_time
+      end
     end
   end
   
@@ -18,27 +20,44 @@ class PositionsController < ApplicationController
   end
 
  
-    def show_tomorrow
-      @date = Date.tomorrow
-      @schedules = Schedule.includes(:cast, :position, :workday)
-                           .where(workdays: { date: @date })
-    end
+  def show_tomorrow
+    @date = Date.tomorrow
+    @positions = Position.all
+    schedules = Schedule.includes(:cast, :position, :workday)
+                        .where(workdays: { date: @date })
+                        .order(:start_time)
+
+    @schedules_by_time_slot = build_schedules_by_time_slot(schedules)
+  end
   
-    def show_day_after_tomorrow
-      @date = Date.today + 2.days
-      @schedules = Schedule.includes(:cast, :position, :workday)
-                           .where(workdays: { date: @date })
-    end
- 
+  def show_day_after_tomorrow
+    @date = Date.today + 2.days
+    @positions = Position.all
+    schedules = Schedule.includes(:cast, :position, :workday)
+                        .where(workdays: { date: @date })
+                        .order(:start_time)
+  
+    @schedules_by_time_slot = build_schedules_by_time_slot(schedules)
+  end
+
     private
 
   def set_date_and_schedules
-    @date ||= Date.today # index では本日、show_tomorrow と show_day_after_tomorrow では上書きされる
+    @date ||= Date.today 
     schedules = Schedule.includes(:position, :cast, :workday)
                         .where(workdays: { date: @date })
                         .order(:start_time)
 
     @schedules_by_time_slot = Schedule::TIME_SLOTS.keys.each_with_object({}) do |time_slot, hash|
+      start_time, end_time = Schedule::TIME_SLOTS[time_slot][1..2]
+      hash[time_slot] = schedules.select do |schedule|
+        schedule.start_time.strftime("%H:%M") == start_time && schedule.end_time.strftime("%H:%M") == end_time
+      end
+    end
+  end
+
+  def build_schedules_by_time_slot(schedules)
+    Schedule::TIME_SLOTS.keys.each_with_object({}) do |time_slot, hash|
       start_time, end_time = Schedule::TIME_SLOTS[time_slot][1..2]
       hash[time_slot] = schedules.select do |schedule|
         schedule.start_time.strftime("%H:%M") == start_time && schedule.end_time.strftime("%H:%M") == end_time
