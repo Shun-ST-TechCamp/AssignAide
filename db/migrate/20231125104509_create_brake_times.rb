257e547f4bc70c3c939eeed165cd32ded424644b
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-26 02:17:52 +0900

    BrakeTimeモデルをマイグレート

--- db/migrate/20231125104509_create_brake_times.rb
+++ db/migrate/20231125104509_create_brake_times.rb
@@ -1,6 +1,9 @@
 class CreateBrakeTimes < ActiveRecord::Migration[7.0]
   def change
     create_table :brake_times do |t|
+      t.integer :min_work_duration
+      t.integer :max_work_duration
+      t.integer :break_duration
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_103452) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_25_104509) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_103452) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "brake_times", charset: "utf8", force: :cascade do |t|
+    t.integer "min_work_duration"
+    t.integer "max_work_duration"
+    t.integer "break_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "casts", charset: "utf8", force: :cascade do |t|
