49c1a76a620ef4c74f051f085e715f5e90193288
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-12-15 11:05:02 +0900

    castをサーバーに追加する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-positions = Position.create([
-  { position_name: 'sara_shiwake', capacity: 2, fatigue_level: 40, position_type: 0, required_skill_level: 2 },
-  { position_name: 'sara_arai', capacity: 1, fatigue_level: 30, position_type: 0, required_skill_level: 2 },
-  { position_name: 'sara_nagashi', capacity: 1, fatigue_level: 20, position_type: 0, required_skill_level: 2 },
-  { position_name: 'sara_huki', capacity: 2, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
-  { position_name: 'kigu_arai', capacity: 1, fatigue_level: 30, position_type: 0, required_skill_level: 2 },
-  { position_name: 'kigu_nagashi', capacity: 1, fatigue_level: 20, position_type: 0, required_skill_level: 2 },
-  { position_name: 'kigu_huki', capacity: 2, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
-  { position_name: 'silver_migaki', capacity: 1, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
-  { position_name: 'extra', capacity: 99, fatigue_level: 0, position_type: 2, required_skill_level: 1 }
-])
+# positions = Position.create([
+#   { position_name: 'sara_shiwake', capacity: 2, fatigue_level: 40, position_type: 0, required_skill_level: 2 },
+#   { position_name: 'sara_arai', capacity: 1, fatigue_level: 30, position_type: 0, required_skill_level: 2 },
+#   { position_name: 'sara_nagashi', capacity: 1, fatigue_level: 20, position_type: 0, required_skill_level: 2 },
+#   { position_name: 'sara_huki', capacity: 2, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
+#   { position_name: 'kigu_arai', capacity: 1, fatigue_level: 30, position_type: 0, required_skill_level: 2 },
+#   { position_name: 'kigu_nagashi', capacity: 1, fatigue_level: 20, position_type: 0, required_skill_level: 2 },
+#   { position_name: 'kigu_huki', capacity: 2, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
+#   { position_name: 'silver_migaki', capacity: 1, fatigue_level: 10, position_type: 1, required_skill_level: 2 },
+#   { position_name: 'extra', capacity: 99, fatigue_level: 0, position_type: 2, required_skill_level: 1 }
+# ])
 
-require 'faker'
-require 'gimei'
+# require 'faker'
+# require 'gimei'
 
-Faker::Config.locale = 'ja'
+# Faker::Config.locale = 'ja'
 
 10.times do
   person = Gimei.name
@@ -36,8 +36,8 @@ Faker::Config.locale = 'ja'
   )
 end
 
-BrakeTime.create!([
-  { min_work_duration: 0, max_work_duration: 360, break_duration: 0 },
-  { min_work_duration: 361, max_work_duration: 480, break_duration: 45 },
-  { min_work_duration: 481, max_work_duration: 1440, break_duration: 60 }
-])
+# BrakeTime.create!([
+#   { min_work_duration: 0, max_work_duration: 360, break_duration: 0 },
+#   { min_work_duration: 361, max_work_duration: 480, break_duration: 45 },
+#   { min_work_duration: 481, max_work_duration: 1440, break_duration: 60 }
+# ])
