bd6ae3b2155cf4ec6189f4fcfc9eb40f9d5c6d1e
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-12-17 02:54:51 +0900

    mage_preview.js:2 Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')のエラーが出るため修正

--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -1,17 +1,23 @@
 document.addEventListener("turbo:load", function() {
-  document.querySelector('#image-input').addEventListener('change', function(e) {
-    let file = e.target.files[0];
-    let reader = new FileReader();
+  const imageInput = document.querySelector('#image-input');
 
-    reader.onload = function(e) {
+  if (imageInput) {
+    imageInput.addEventListener('change', function(e) {
+      let file = e.target.files[0];
+      let reader = new FileReader();
       let preview = document.querySelector('#image-preview');
-      preview.innerHTML = '<img src="' + e.target.result + '" class="preview-image" style="max-width: 200px; max-height: 200px;" />';
-    };
 
-    if (file) {
-      reader.readAsDataURL(file);
-    } else {
-      preview.innerHTML = '';
-    }
-  });
+      reader.onload = function(e) {
+        if (preview) {
+          preview.innerHTML = '<img src="' + e.target.result + '" class="preview-image" style="max-width: 200px; max-height: 200px;" />';
+        }
+      };
+
+      if (file) {
+        reader.readAsDataURL(file);
+      } else if (preview) {
+        preview.innerHTML = '';
+      }
+    });
+  }
 });
